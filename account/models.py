dc1495ac172451fd04303833465ca8019b923e40
zer0who <eagle7449@naver.com>
2022-08-28 01:50:02 +0900

    유저 프로젝트 생성, account 앱 db 컬럼 수정

--- account/models.py
+++ account/models.py
@@ -6,6 +6,7 @@ class Account_info(models.Model):
     email = models.EmailField(max_length=50, null=True)
     password = models.CharField(max_length=100)
     openstack_user_id = models.CharField(max_length=100, null=True)
+    openstack_user_project_id = models.CharField(max_length=100, null=True)
     # token = models.CharField(max_length=200, null=True)
     # api_key = models.CharField(max_length=200)
     # secret_key = models.CharField(max_length=200)
