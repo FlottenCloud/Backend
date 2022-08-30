b4404493f40c65a7044904bdb2c3cc1e7fd34b3e
zer0who <eagle7449@naver.com>
2022-08-31 01:08:57 +0900

    fix

--- cloudmanager/settings.py
+++ cloudmanager/settings.py
@@ -141,7 +141,7 @@ CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     # 허용할 Origin 추가
-    "http://localhost:3000/",
+    "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
 CORS_ORIGIN_ALLOW_ALL = True
