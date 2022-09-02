9b16238bce42d4fb19a454920c855cf2213458b8
zer0who <eagle7449@naver.com>
2022-09-02 17:05:08 +0900

    pillow 설치

---
+++ template_modifier.py
@@ -0,0 +1,25 @@
+import json
+
+def templateImageModify(template, user_stack_num, flavor):  # template 파일, 유저가 생성할 스택(인스턴스)의 번호(서수) -> 스택 번호 말고 이름 등을 받아올 수도 있음.
+    with open(template, 'r') as f:
+        template_data = json.load(f)
+    template_data["stack_name"] = template_data["stack_name"] + str(user_stack_num)
+    template_data["template"]["resources"]["mybox"]["properties"]["name"] = template_data["template"]["resources"]["mybox"]["properties"]["name"] + str(user_stack_num)
+    template_data["template"]["resources"]["mybox"]["properties"]["flavor"] = flavor
+    print(json.dumps(template_data))
+
+    # return(template_data)
+
+def templateFlavorModify(template, user_stack_num):
+    with open(template, 'r') as f:
+        template_data = json.load(f)
+    
+    template_data["stack_name"] = template_data["stack_name"] + str(user_stack_num)
+    print(json.dumps(template_data))
+
+# def templateImageModify(template):
+#     with open(template, 'r') as f:
+#         template_data = json.load(f)
+#     print(json.dumps(template_data))
+
+templateImageModify('templates\centos.json', 2)
