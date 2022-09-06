336b449f45e8fe48005e40047238df5d628b821b
zer0who <eagle7449@naver.com>
2022-09-07 03:17:13 +0900

    백업 이미지 DB 정보 이미지 당 하나만 남게 임시방편으로 수정

--- fileBoard/models.py
+++ fileBoard/models.py
@@ -6,5 +6,6 @@ class InstanceImgBoard(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     instance_img_file = models.FileField(max_length=255, blank=True, upload_to="img-files")
+    
     class Meta:
         db_table = 'fileBoard'
--- openstack/updater.py
+++ openstack/updater.py
@@ -6,21 +6,24 @@ import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 from openstack.models import OpenstackBackupImage, OpenstackInstance
 from openstack.serializers import OpenstackBackupImageSerializer
+from fileBoard.models import InstanceImgBoard
 
+def imageSaver():
+    pass
 
-def backup6():
-    print("this function runs every 60 seconds")
-    import openstack_controller as oc                            # import는 여기 고정
+def backup(cycle):
+    print("this function runs every", cycle, "seconds")
+    import openstack_controller as oc                            # import는 여기 고정 -> 컴파일 시간에 circular import 때문에 걸려서
     openstack_hostIP = oc.hostIP
 
     try:
-        instance_count = OpenstackInstance.objects.filter(backup_time=6).count()
+        instance_count = OpenstackInstance.objects.filter(backup_time=cycle).count()
         if instance_count == 0:
-            return print("백업 주기 6시간짜리 instance 없음")
+            return "백업 주기 ", cycle, "시간짜리 instance 없음"
 
         token = oc.admin_token()
-        backup_instance_list  = OpenstackInstance.objects.filter(backup_time=6)
-        print("6시간짜리 리스트: ", backup_instance_list)
+        backup_instance_list = OpenstackInstance.objects.filter(backup_time=cycle)
+        print(cycle, "시간짜리 리스트: ", backup_instance_list)
 
         for instance in backup_instance_list:
             print("인스턴스 오브젝트: ", instance)
@@ -43,22 +46,6 @@ def backup6():
             instance_image_ID = instance_image_URL.split("/")[6]
             print("image_ID : " + instance_image_ID)
 
-            backup_image_data = {
-                "instance_id" : backup_instance_id,
-                "image_id" : instance_image_ID,
-                "image_url" : instance_image_URL
-            }
-
-            serializer = OpenstackBackupImageSerializer(data=backup_image_data)
-    
-            if serializer.is_valid():
-                serializer.save()
-                print("saved")
-                print(serializer.data)
-            else:
-                print("not saved")
-                print(serializer.errors)
-
             while(True):
                 image_status_req = requests.get("http://" + openstack_hostIP + "/image/v2/images/" + instance_image_ID,
                 headers = {"X-Auth-Token" : token})
@@ -74,86 +61,55 @@ def backup6():
             file.write(image_download_req.content)
             file.close()
 
-            print("image file download response is", backup_req)
-    
-    except OperationalError:
-        return print("인스턴스가 없습니다.")
-
-
-def backup12():
-    print("this function runs every 60 seconds")
-    import openstack_controller as oc                            # import는 여기 고정
-    openstack_hostIP = oc.hostIP
-
-    try:
-        instance_count = OpenstackInstance.objects.filter(backup_time=12).count()
-        if instance_count == 0:
-            return print("백업 주기 12시간짜리 instance 없음")
-
-        token = oc.admin_token()
-        backup_instance_list = OpenstackInstance.objects.filter(backup_time=12)
-        print("6시간짜리 리스트: ", backup_instance_list)
-
-        for instance in backup_instance_list:
-            print("인스턴스 오브젝트: ", instance)
-            backup_instance_id = instance.instance_id
-            print("인스턴스 id: ", backup_instance_id)
-            backup_payload = {
-                "createBackup": {
-                    "name": "Backup " + backup_instance_id,
-                    "backup_type": "daily",
-                    "rotation": 1
-                }
-            }
-            backup_req = requests.post("http://" + openstack_hostIP + "/compute/v2.1/servers/" +
-                backup_instance_id + "/action",
-                headers={"X-Auth-Token": token},    # admin토큰임 ㅋㅋ
-                data=json.dumps(backup_payload))
-
-            instance_image_ID = backup_req.headers["Location"]
-            print("image_URL : " + instance_image_ID)
-            instance_image_URL = instance_image_ID.split("/")[6]
-            print("image_ID : " + instance_image_URL)
-
-
             backup_image_data = {
                 "instance_id" : backup_instance_id,
                 "image_id" : instance_image_ID,
-                "image_url" : instance_image_URL
+                "image_url" : instance_image_ID
             }
 
-            serializer = OpenstackBackupImageSerializer(data=backup_image_data)
-    
-            if serializer.is_valid():
-                serializer.save()
-                print("saved")
-                print(serializer.data)
-            else:
-                print("not saved")
-                print(serializer.errors)
+            print(backup_image_data)
 
-            while(True):
-                image_status_req = requests.get("http://" + openstack_hostIP + "/image/v2/images/" + instance_image_URL,
-                headers = {"X-Auth-Token" : token})
-                print("이미지 상태 조회 리스폰스: ", image_status_req.json())
-                image_status = image_status_req.json()["status"]
-                if image_status == "active":
-                    break
-                time.sleep(2)
+            if OpenstackBackupImage.objects.filter(instance_id=backup_instance_id).count() == 0:   # 해당 이미지가 DB에 저장 안돼있으면 create()
+                serializer = OpenstackBackupImageSerializer(data=backup_image_data)
+                if serializer.is_valid():
+                    serializer.save()
+                    print("saved image info")
+                    print(serializer.data)
+                else:
+                    print("not saved")
+                    print(serializer.errors)
 
-            image_download_req = requests.get("http://" + openstack_hostIP + "/image/v2/images/" + instance_image_URL + "/file",
-                headers = {"X-Auth-Token" : token})
-            file = open("C:/Users/YOUNGHOO KIM/Desktop/PNU/Graduation/codes/cloudmanager/back_imgs/" + backup_instance_id + ".qcow2", "wb")
-            file.write(image_download_req.content)
-            file.close()
+            else:   # 해당 이미지가 DB에 저장돼있으면 update()
+                serializer = OpenstackBackupImageSerializer(data=backup_image_data)
+                OpenstackBackupImage.objects.get(instance_id=backup_instance_id).delete()   # 시리얼라이저 업데이트 구현이 아직 안돼서 이렇게 구현
+                if serializer.is_valid():
+                    serializer.save()
+                    print("updated image info")
+                    print(serializer.data)
+                else:
+                    print("not updated")
+                    print(serializer.errors)
 
-            print("image file download response is", backup_req)
+            return "image file download response is ", backup_req
 
     except OperationalError:
-            return print("인스턴스가 없습니다.")
+            return "인스턴스가 없습니다."
+
+def backup6():
+    backup_res = backup(6)
+    print(backup_res)
+
+
+def backup12():
+    backup_res = backup(12)
+    print(backup_res)
+
+def deleter():
+    OpenstackBackupImage.objects.all().delete()
 
 def start():
     scheduler = BackgroundScheduler()
+    # scheduler.add_job(deleter, 'interval', seconds=5)
     scheduler.add_job(backup6, 'interval', seconds=30)
     scheduler.add_job(backup12, 'interval', seconds=60)
     
