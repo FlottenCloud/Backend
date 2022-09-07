46932e161d9edead20aeb15b3377971d35e0f371
zer0who <eagle7449@naver.com>
2022-09-08 01:50:52 +0900

    fix

--- fileBoard/models.py
+++ fileBoard/models.py
@@ -6,7 +6,7 @@ class InstanceImgBoard(models.Model):
     # Foreign key(instance - image-file)
     instance_id = models.ForeignKey("openstack.OpenstackInstance", related_name="instance_backup_img_file", on_delete=models.CASCADE, db_column="instance_id")
     # backup image file info
-    image_id = models.CharField(max_length = 50, primary_key = True)
+    image_id = models.CharField(max_length=50, primary_key=True)
     instance_img_file = models.FileField(max_length=255, blank=True, upload_to="img-files")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
--- openstack/models.py
+++ openstack/models.py
@@ -13,14 +13,14 @@ class OpenstackInstance(models.Model):  #유저와 연관짓기 위한 외래키
     # Foreign key(user - stack info)
     user_id = models.ForeignKey("account.Account_info", related_name="user_resource_info", on_delete=models.CASCADE, db_column="user_id")
     # stack info
-    instance_id = models.CharField(max_length = 50, primary_key = True) # backup image에서 외래키로 참조
-    instance_name = models.CharField(max_length = 50)
-    stack_id = models.CharField(max_length = 50)
-    stack_name = models.CharField(max_length = 50)
+    instance_id = models.CharField(max_length=50, primary_key=True) # backup image에서 외래키로 참조
+    instance_name = models.CharField(max_length=50)
+    stack_id = models.CharField(max_length=50)
+    stack_name = models.CharField(max_length=50)
     ip_address = models.GenericIPAddressField()
-    status = models.CharField(max_length = 50)
-    image_name = models.CharField(max_length = 50)
-    flavor_name = models.CharField(max_length = 50)
+    status = models.CharField(max_length=50)
+    image_name = models.CharField(max_length=50)
+    flavor_name = models.CharField(max_length=50)
     ram_size = models.FloatField(validators=[MaxValueValidator(12)])
     disk_size = models.FloatField(validators=[MaxValueValidator(100)])
     num_cpu = models.IntegerField(validators=[MaxValueValidator(12)])
@@ -30,5 +30,5 @@ class OpenstackBackupImage(models.Model):
     # Foreign key(instance - backup image)
     instance_id = models.ForeignKey("OpenstackInstance", related_name="instance_backup_img", on_delete=models.CASCADE, db_column="instance_id")
     # backup image info
-    image_id = models.CharField(max_length = 50, primary_key = True)
-    image_url = models.CharField(max_length = 100)
+    image_id = models.CharField(max_length=50, primary_key=True)
+    image_url = models.CharField(max_length=100)
