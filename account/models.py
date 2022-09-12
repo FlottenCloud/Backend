bc15a8032f0acb83db634b0b184a614ad3e438b1
zer0who <eagle7449@naver.com>
2022-09-12 18:36:48 +0900

    유저 생성시 클라우드스택 네트워크 생성, 유저 타입 root admin으로

--- account/models.py
+++ account/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from django.core.validators import MinValueValidator
 # Create your models here.
 
 class AccountInfo(models.Model):
@@ -11,6 +13,8 @@ class AccountInfo(models.Model):
     cloudstack_account_id = models.CharField(max_length=100)
     cloudstack_apiKey = models.CharField(max_length=200)
     cloudstack_secretKey = models.CharField(max_length=200)
+    cloudstack_network_id = models.CharField(max_length=200)
+    cloudstack_network_vlan = models.IntegerField(validators=[MinValueValidator(100)])
 
     class Meta:
         db_table = 'accounts_info'
