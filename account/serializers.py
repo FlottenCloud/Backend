78ce6b0e100b0f91443856c35a4a4eef3410154d
unknown <eagle7449@naver.com>
2022-09-15 17:13:22 +0900

    fix

--- account/serializers.py
+++ account/serializers.py
@@ -10,11 +10,11 @@ class UserRegisterSerializer(serializers.Serializer):
     email = serializers.EmailField(help_text="User's email")
     password = serializers.CharField(help_text="User's password")
 
-class UserInfoSeializer(serializers.Serializer):
-    user_id = serializers.CharField(help_text="User's ID")
-    email = serializers.EmailField(help_text="User's email")
-    first_name = serializers.CharField(help_text="User's first name")
-    last_name = serializers.CharField(help_text="User's last name")
+# class UserInfoSeializer(serializers.Serializer):
+#     user_id = serializers.CharField(help_text="User's ID")
+#     email = serializers.EmailField(help_text="User's email")
+#     first_name = serializers.CharField(help_text="User's first name")
+#     last_name = serializers.CharField(help_text="User's last name")
 
 class UserDeleteSerializer(serializers.Serializer):
     user_id = serializers.CharField(help_text="User's ID")
