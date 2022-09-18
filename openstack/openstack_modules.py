c4ddfcffe37f77ad02ee8281999b1d632f14eb63
leebonghun <drok02@naver.com>
2022-09-19 02:57:50 +0900

    template modifier fix

--- openstack/openstack_modules.py
+++ openstack/openstack_modules.py
@@ -132,13 +132,14 @@ class TemplateModifier:
         template_data["stack_name"] = str(user_instance_name)   # 스택 name 설정
         template_data["template"]["resources"]["mybox"]["properties"]["name"] = str(user_instance_name) # 인스턴스 name 설정
         # template_data["template"]["resources"]["demo_key"]["properties"]["name"] = user_id + "_" + user_instance_name  # 키페어 name 설정
-        template_data["template"]["resources"]["server_floating_ip"]["properties"]["floating_network_id"] = oc.public_network_id
-        template_data["template"]["resources"]["mynet"]["properties"]["name"] = user_id + "-net" + user_instance_name    # 네트워크 name 설정
-        template_data["template"]["resources"]["mysub_net"]["properties"]["name"] = user_id + "-subnet" + user_instance_name # sub네트워크 name 설정
-        template_data["template"]["resources"]["router_gateway"]["properties"]["network_id"] = oc.public_network_id
-        template_data["template"]["resources"]["mysecurity_group"]["properties"]["name"] = user_id + "-security_group" + user_instance_name # 보안그룹 name 설정
+        # template_data["template"]["resources"]["server_floating_ip"]["properties"]["floating_network_id"] = oc.public_network_id
+        # template_data["template"]["resources"]["mynet"]["properties"]["name"] = user_id + "-net" + user_instance_name    # 네트워크 name 설정
+        # template_data["template"]["resources"]["mysub_net"]["properties"]["name"] = user_id + "-subnet" + user_instance_name # sub네트워크 name 설정
+        # template_data["template"]["resources"]["router_gateway"]["properties"]["network_id"] = oc.public_network_id
+        # template_data["template"]["resources"]["mysecurity_group"]["properties"]["name"] = user_id + "-security_group" + user_instance_name # 보안그룹 name 설정
         template_data["parameters"]["flavor"] = flavor    # flavor 설정
         template_data["parameters"]["packages"] = user_package    # package 설정
+        template_data["parameters"]["network_id"] = oc.public_network_id
         print(json.dumps(template_data))
 
         return(json.dumps(template_data))
@@ -148,13 +149,14 @@ class TemplateModifier:
         template_data["stack_name"] = str(user_instance_name)   # 스택 name 설정
         template_data["template"]["resources"]["mybox"]["properties"]["name"] = str(user_instance_name)# 인스턴스 name 설정
         # template_data["template"]["resources"]["demo_key"]["properties"]["name"] = user_id + "_" + user_instance_name  # 키페어 name 설정
-        template_data["template"]["resources"]["server_floating_ip"]["properties"]["floating_network_id"] = oc.public_network_id
-        template_data["template"]["resources"]["mynet"]["properties"]["name"] = user_id + "-net" + user_instance_name    # 네트워크 name 설정
-        template_data["template"]["resources"]["mysub_net"]["properties"]["name"] = user_id + "-subnet" + user_instance_name # sub네트워크 name 설정
-        template_data["template"]["resources"]["router_gateway"]["properties"]["network_id"] = oc.public_network_id
-        template_data["template"]["resources"]["mysecurity_group"]["properties"]["name"] = user_id + "-security_group" + user_instance_name # 보안그룹 name 설정
+        # template_data["template"]["resources"]["server_floating_ip"]["properties"]["floating_network_id"] = oc.public_network_id
+        # template_data["template"]["resources"]["mynet"]["properties"]["name"] = user_id + "-net" + user_instance_name    # 네트워크 name 설정
+        # template_data["template"]["resources"]["mysub_net"]["properties"]["name"] = user_id + "-subnet" + user_instance_name # sub네트워크 name 설정
+        # template_data["template"]["resources"]["router_gateway"]["properties"]["network_id"] = oc.public_network_id
+        # template_data["template"]["resources"]["mysecurity_group"]["properties"]["name"] = user_id + "-security_group" + user_instance_name # 보안그룹 name 설정
         template_data["parameters"]["image"] = backup_img_name  # 백업해놓은 이미지로 img 설정
         template_data["parameters"]["flavor"] = flavor    # flavor 설정
+        template_data["parameters"]["network_id"] = oc.public_network_id
         
         print(json.dumps(template_data))
 
