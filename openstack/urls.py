ef455142059046d336b6426f74a03c02b2c84d36
zer0who <eagle7449@naver.com>
2022-09-27 21:47:38 +0900

    fix

--- openstack/urls.py
+++ openstack/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.Openstack.as_view()),    # 인스턴스 페이지 관련
     path('<int:instance_pk>/', views.InstanceInfo.as_view()),
-    path('log/<int:instance_pk>/', views.InstanceLog.as_view()),
+    path('log/<int:instance_pk>/', views.InstanceLogShower.as_view()),
     path('dashboard/', views.DashBoard.as_view()),  # dashboard 관련
     path('instance-start/', views.InstanceStart.as_view()), # 인스턴스 start btn에 붙일 url
     path('instance-stop/', views.InstanceStop.as_view()),   # 인스턴스 stop btn에 붙일 url
