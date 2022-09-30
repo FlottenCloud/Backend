8ada8319ddeff082b853e2db11b9c9ee0d02236e
zer0who <eagle7449@naver.com>
2022-10-01 02:04:39 +0900

    fix

--- infosender/apps.py
+++ infosender/apps.py
@@ -5,5 +5,5 @@ class InfosenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'infosender'
 
-    def ready():
+    def ready(self):
         import infosender.signals
