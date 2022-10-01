82e0c1cbdbcbd7c1a6acde2742dcfa1e3f60da80
zer0who <eagle7449@naver.com>
2022-10-01 14:29:03 +0900

    fixed routing url

--- infosender/consumers.py
+++ infosender/consumers.py
@@ -4,7 +4,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class InfoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        user_id = self.scope["user"]
+        user_id = self.scope["url_route"]["kwargs"]["user_id"]
         print(user_id)
         self.group_name = "user-{}".format(user_id)
         print(self.group_name)
--- infosender/routing.py
+++ infosender/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server/', consumers.InfoConsumer.as_asgi())
+    re_path(r'ws/socket-server/<str:user_id>/', consumers.InfoConsumer.as_asgi())
 ]
